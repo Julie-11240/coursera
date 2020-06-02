2fe10677fb3922357884fd7dc7a6ed26820c7f4b
julie-11240 <julia.jasy.black@gmail.com>
2020-06-02 21:09:47 +0300

    module 4

---
+++ module4-harder/SpeakHello.js
@@ -0,0 +1,13 @@
+(function (window) {
+
+var helloSpeaker = {};
+
+var speakWord = "Hello";
+helloSpeaker.speak = function (){
+	console.log(speakWord + " " + helloSpeaker.name);
+}
+
+
+window.helloSpeaker = helloSpeaker;
+
+}) (window);
